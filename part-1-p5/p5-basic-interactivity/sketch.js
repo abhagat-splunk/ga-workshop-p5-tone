351c1498535af33c41ec683749500721a73c861a
rose-water <rachel.ulgado@gmail.com>
2020-08-04 19:34:58 -0700

    basic-interactivity

---
+++ part-1-p5/p5-basic-interactivity/sketch.js
@@ -0,0 +1,36 @@
+// p5-basic-interactivity
+
+
+
+
+
+let diameter;
+let r, g, b;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  diameter = 40;
+  pickRandomColor();
+}
+
+function draw() {
+  background(250, 200, 255);
+  noStroke();
+  fill(r, g, b);
+  
+  ellipse(mouseX, mouseY, diameter, diameter);
+}
+
+function mousePressed() {
+  diameter = random(40, 180);
+}
+
+function keyPressed() {
+  pickRandomColor();
+}
+
+function pickRandomColor() {
+  r = random(255);
+  g = random(255);
+  b = random(255);
+}
