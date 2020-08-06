469ec311d839844ccde0630172dadd710b6776d1
rose-water <rachel.ulgado@gmail.com>
2020-08-05 19:36:16 -0700

    synth and oscillator

---
+++ part-2-tone/tone-basic-oscillator/sketch.js
@@ -0,0 +1,43 @@
+// tone-basic-oscillator
+
+let osc;
+let isOn = false;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+
+  // Four basic types of waves: 
+  // sine, square, sawtooth, triangle
+  // Sine wave at 440hz = A4
+  osc = new Tone.Oscillator(440, "sine").toDestination();
+}
+
+function draw() {
+  background(0);
+
+  fill(255);
+  textSize(130);
+  textAlign(CENTER);
+
+  if (isOn) {
+    text('ON', width/2, height/2);
+  } else {
+    text('OFF', width/2, height/2)
+  }
+}
+
+function mousePressed() {
+  if (isOn) {
+    osc.stop();
+  } else {
+    osc.start();
+  }
+
+  isOn = !isOn;
+}
+
+
+
+
+
---
+++ part-2-tone/tone-basic-synth/sketch.js
@@ -0,0 +1,19 @@
+// tone-basic-synth
+
+let synth;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+
+  synth = new Tone.Synth().toDestination();
+}
+
+function draw() {
+  background(0);
+}
+
+function mousePressed() {
+  ellipse(mouseX, mouseY, 20, 20);
+  synth.triggerAttackRelease("C4", "8n");
+}
