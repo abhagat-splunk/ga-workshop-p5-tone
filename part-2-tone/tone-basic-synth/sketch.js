6aaf6b9dafaffac05cf4482ca49c78789c6c208e
rose-water <rachel.ulgado@gmail.com>
2020-08-07 16:01:36 -0700

    last updates

--- part-2-tone/tone-basic-synth/sketch.js
+++ part-2-tone/tone-basic-synth/sketch.js
@@ -5,15 +5,19 @@ let synth;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
+  noStroke();
 
   synth = new Tone.Synth().toDestination();
 }
 
 function draw() {
   background(0);
+  if ( mouseIsPressed ) {
+    fill(255, 200, 255);
+    ellipse(mouseX, mouseY, 100, 100);
+  }
 }
 
 function mousePressed() {
-  ellipse(mouseX, mouseY, 20, 20);
   synth.triggerAttackRelease("C4", "8n");
 }
