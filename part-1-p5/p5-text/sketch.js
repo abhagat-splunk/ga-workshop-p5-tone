8e6e60791ec5bb85667123ad965009febe7342ca
rose-water <rachel.ulgado@gmail.com>
2020-08-06 19:20:10 -0700

    updating p5 ones

---
+++ part-1-p5/p5-text/sketch.js
@@ -0,0 +1,26 @@
+// p5-text
+
+let robotoMono;
+
+function preload() {
+  robotoMono = loadFont('RobotoMono.ttf');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+  // setup our font so we can use it
+  textFont(robotoMono);
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+
+function draw() {
+  background(0);
+  fill(0, 255, 255);
+  text(key, width/2, height/2);
+}
+
+
+
+
--- part-2-tone/tone-synth-keys/sketch.js
+++ part-2-tone/tone-synth-keys/sketch.js
@@ -1,1 +1,28 @@
 // tone-synth-keys
+
+// Basic mousePressed with Tone.js
+// a minor scale played sequentially on click
+let synth;
+let idx = 0;
+
+const AMinorScale = ['A3', 'B3', 'C4', 'D4', 'E4', 'F4', 'G4'];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  synth = new Tone.Synth().toDestination()
+}
+
+function draw() {
+  background(0);
+}
+
+function mousePressed() {
+
+  // Current audio context time (in seconds)
+  const now = Tone.now();
+
+  AMinorScale.forEach((note, idx) => {
+    synth.triggerAttackRelease(note, '16n', now + idx);
+  });
+}
